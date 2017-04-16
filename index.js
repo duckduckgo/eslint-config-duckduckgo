361733225764607cd4dc44c9c8ecf5c67bc543b4
Bradley Braithwaite <bradley@duckduckgo.com>
2017-04-15 20:44:23 -0400

    activate possible errors and best practice rules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: [
         "./environments",
         "./globals",
-        "./rules/jshint.js"
+        "./rules/jshint.js",
+        "./rules/possible-errors.js",
+        "./rules/best-practices.js"
     ].map(require.resolve)
 };
